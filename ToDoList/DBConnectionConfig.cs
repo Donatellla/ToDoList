63afa2a636bc68c9eb2a65d2768dc0ba7f292ad7
slavasergeev <youail>
2023-03-23 22:33:29 +0700

    Add DBConfig template

---
+++ ToDoList/DBConnectionConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class DBConnectionConfig
+    {
+        public static string host = "default";
+        public static int port = 3306;
+        public static string database = "default";
+        public static string username = "default";
+        public static string password = "default";
+    }
+}
